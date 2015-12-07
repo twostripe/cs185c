3c2d441184da799ada26a3b85f8db593462829d1
twostripe <macarpenter@gmail.com>
2015-12-07 03:47:12 -0800

    added so many things
    mainly many many playlist generators and ways of comparing them. and
    graphs!

--- assignment-05/create_playlist.py
+++ assignment-05/create_playlist.py
@@ -5,6 +5,8 @@ import	sys
 import	warnings
 import	threading	as	th
 from random import randint
+from random import random
+import matplotlib.pyplot as plt
 
 import numpy as np
 
@@ -13,9 +15,13 @@ import os
 dir = "D:/Mark's Temp Folder/github/cs185c/assignment-05/"
   
 songs = np.array(list(csv.reader(open(os.path.join(dir, "songs.csv"), 'rt'))))
+#songs = np.array(list(csv.reader(open(os.path.join(dir, "songs_limited.csv"), 'rt'))))
 header = np.array(list(csv.reader(open(os.path.join(dir, "header.csv"), 'rt'))))
 
-       
+song_ids = []
+for song in songs:
+    song_ids.append(song[1])
+                 
 def build_playlist(genre, num_songs):
     # the genre we're looking for
     # (change this)
@@ -34,42 +40,47 @@ def build_playlist(genre, num_songs):
     for i in range(0,num_songs) :
         playlist.append(genre_list[randint(0,len(genre_list)-1)])
         
-    print("creating '" + genre + "' playlist:")
-    
-    for the_song in playlist :
-        # try and match it to a song in the echonest database
-        # but don't try too hard     
-            
-        print (the_song['id'] + ": " + the_song['name'] + " -- " + the_song['artist'])
-
-    print ("")
+#    print("creating '" + genre + "' playlist")
     
     return playlist
 
+# builds a collection of playlists
+def build_playlist_set(genre_1, genre_proportion_1, genre_2, genre_proportion_2):
+    num_playlists = 20
+    playlist_set = []
 
-master_playlist = []
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
-master_playlist.append(build_playlist("hip-hop", 15))
+    proportion_total = genre_proportion_1 + genre_proportion_2
+
+    int = 0
+    for i in range(0,20): 
+        genre = ""
+        if (proportion_total * random() < genre_proportion_1):
+            genre = genre_1
+        else:
+            genre = genre_2
+        
+        playlist_set.append(build_playlist(genre, 15))
+        
+    return playlist_set
+
+
+
+# make a few collections of playlists with varying amounts of "hip-hop" and "pop"
+playlists_collection = {}
+playlists_collection['0z'] = build_playlist_set("hip-hop", 0, "pop", 1)
+playlists_collection['Alfrig'] = build_playlist_set("hip-hop", .1, "pop", .9)
+playlists_collection['Barney'] = build_playlist_set("hip-hop", .2, "pop", .8)
+playlists_collection['Cynthia'] = build_playlist_set("hip-hop", .3, "pop", .7)
+playlists_collection['Dennerys'] = build_playlist_set("hip-hop", .4, "pop", .6)
+playlists_collection['Elsie'] = build_playlist_set("hip-hop", .5, "pop", .5)
+playlists_collection['Fenna'] = build_playlist_set("hip-hop", .6, "pop", .4)
+playlists_collection['Greggory'] = build_playlist_set("hip-hop", .7, "pop", .3)
+playlists_collection['Hal'] = build_playlist_set("hip-hop", .8, "pop", .2)
+playlists_collection['IO'] = build_playlist_set("hip-hop", .9, "pop", .1)
+playlists_collection['Joan'] = build_playlist_set("hip-hop", 1, "pop", 0)
 
 # build "master list"
-# mostly to see if we're getting some repeats
+# generate "taste" for each person
 # (we are!)
 def build_master_list(master_playlist):
     # master_playlist should be a list of playlistslists, each containing a song
@@ -84,9 +95,31 @@ def build_master_list(master_playlist):
             else: 
                 master_list[song['id']] = 1
             
-            
-            
-    for song in master_list: print(song + ", " + str(master_list[song]))
-    
-build_master_list(master_playlist)
-        
+#    for song in master_list: print(song + ", " + str(master_list[song]))
+    return master_list
+
+# buil "master" taste lists for each artist
+taste_list = {}
+for person in playlists_collection:
+    taste_list[person] = build_master_list(playlists_collection[person])
+
+def plot_taste(taste, type):
+    plt.plot(range(len(taste)), taste.values(), type)
+
+# we can use this "taste" list to see a DJ's taste
+plot_taste(taste_list['Alfrig'], 'r+')
+
+# make a new taste
+my_taste = build_master_list(build_playlist_set("hip-hop", 0, "pop", 1))
+plot_taste(my_taste, 'b+')
+
+# find where the two tastes share a song
+shared_taste = {}
+for song in my_taste: 
+    if (song in taste_list['Alfrig']): 
+        shared_taste[song] = my_taste[song]
+
+plot_taste(shared_taste, 'go')
+
+plt.ylim(0, 4)
+plt.show()
