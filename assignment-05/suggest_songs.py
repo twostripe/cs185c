1fb9205e682d222796dfd543afb27bdac1400f4d
twostripe <macarpenter@gmail.com>
2015-12-09 05:00:14 -0800

    made changes ready to submit

--- assignment-05/suggest_songs.py
+++ assignment-05/suggest_songs.py
@@ -1,5 +1,3 @@
-# creates a playlist with songs from the same genre
-
 import	csv
 import	sys
 import	warnings
@@ -13,18 +11,26 @@ import numpy as np
 
 import os
 
-plt.close()
+## CREATE music collections
+# collection = songs from a set of playlists, with playcount
+# playlist = list of songs (no repetitions)
 
 dir = ""
-  
-#songs = np.array(list(csv.reader(open(os.path.join(dir, "songs.csv"), 'rt'))))
-songs = np.array(list(csv.reader(open(os.path.join(dir, "songs_limited.csv"), 'rt'))))
+song_list = np.array(list(csv.reader(open(os.path.join(dir, "songs.csv"), 'rt'))))
+#song_list = np.array(list(csv.reader(open(os.path.join(dir, "songs_limited.csv"), 'rt')))) # a limited subset of the songs to make things quicker and more impressive (more chance for repetition)
 header = np.array(list(csv.reader(open(os.path.join(dir, "header.csv"), 'rt'))))
 
+songs = collections.OrderedDict()
 songs_by_id = {}
-for song in songs:
+for song in song_list:
+    songs[song[1]] = {'name': song[3], 'artist': song[2]}
     songs_by_id[song[1]] = {'id': song[1], 'name': song[3], 'artist': song[2]}
-                 
+
+
+
+### FUNCTION DEFINITIONS
+
+## returns a playlist of a specific genre of a specified number of songs
 def build_playlist(genre, num_songs):
     # the genre we're looking for
     # (change this)
@@ -32,22 +38,19 @@ def build_playlist(genre, num_songs):
     
     # put all the songs of a particular genre to one side
     i = 0
-    for the_song in songs :
+    for the_song in song_list :
         if the_song[0] == genre :
             genre_list.append({'id': the_song[1], 'name': the_song[3], 'artist': the_song[2]})
 
-    # pick 10 random songs from this list
     playlist = []
     
     i = 0
     for i in range(0,num_songs) :
         playlist.append(genre_list[randint(0,len(genre_list)-1)])
         
-#    print("creating '" + genre + "' playlist")
-    
     return playlist
 
-# builds a collection of playlists
+# builds a collection of playlists with two genres of a specified proportion
 def build_playlist_set(genre_1, genre_proportion_1, genre_2, genre_proportion_2):
     num_playlists = 20
     playlist_set = []
@@ -119,10 +122,10 @@ def plot_tastes(taste_a, taste_b):
     
     max_value = 0
 
-    data_a = collections.OrderedDict(songs_by_id)
-    data_b = collections.OrderedDict(songs_by_id)
-    data_shared_a = collections.OrderedDict(songs_by_id)
-    data_shared_b = collections.OrderedDict(songs_by_id)
+    data_a = collections.OrderedDict(songs)
+    data_b = collections.OrderedDict(songs)
+    data_shared_a = collections.OrderedDict(songs)
+    data_shared_b = collections.OrderedDict(songs)
 
     for song in data_a:
         data_a[song] = np.nan
@@ -162,35 +165,47 @@ def plot_tastes(taste_a, taste_b):
     
     plt.ylim(ymin = 0)
     plt.ylim(ymax = max_value + 1)
+    plt.xlim(xmin = 0)
+    plt.xlim(xmax = len(songs))
     frame.xaxis.set_ticks_position('none');
     frame.yaxis.set_ticks_position('left')
+    plt.xlabel("songs from MSD")
+    plt.ylabel("occurance in the collection")
+    plt.title("Occurance of a song in 'our collection' and the 'most similar collection'")
     plt.show()
 
-# make a new taste
-my_taste = build_master_list(build_playlist_set("hip-hop", 0.9, "pop", 0.1))
-#my_taste = build_master_list(build_playlist_set("hip-hop", 0.1, "pop", 0.9))
-
-
-
 # figure out how similar taste_b is to tast_a
-# returns a number > 0
-# not necessarily <= 1
-# ie, 1.0 is not 100% similar ... i think
+# returns a number: 0 <= n <= 1
+# ie, 1.0 is 100% somilar
+# also, even if the two tastes share all the same songs, the song weights (play counts) matter as well, so there is still varience then
 def find_similarity(taste_a, taste_b):
-    # this is basically the same as multiplying them together if they were sparse matrices
+    # this is basically the same as dividing them together if they were sparse matrices
+    # (i think)
     weights = []
-    for song in taste_a:
-        if (song in taste_b):
-            weights.append(taste_a[song] * taste_b[song])
-        else:
+    overlaps = 0
+    for song in songs:
+        # if song is in one but not the other = 0
+        # 
+        if ((song in taste_a) and (song in taste_b)):
+            a_count = taste_a[song]
+            b_count = taste_b[song]
+            weight = a_count / b_count if a_count < b_count else b_count / a_count
+            weights.append(weight)
+            overlaps += 1
+        elif ((song in taste_a) or (song in taste_b)):
             weights.append(0)
             
+#    print ("overlaps: " + str(overlaps))
     return np.mean(weights)
+
+# make a new taste
+#my_taste = build_master_list(build_playlist_set("hip-hop", 0.9, "pop", 0.1))
+my_taste = build_master_list(build_playlist_set("hip-hop", 0.5, "pop", 0.5))
+#my_taste = build_master_list(build_playlist_set("hip-hop", 0.1, "pop", 0.9))
 
 largest_similarity = 0
 most_similar_taste = {}
 most_similar_profile = ""
-#print ("similarity with '"+ "base" +"': " + str(find_similarity(my_taste, my_taste)))
 for profile in taste_list:
     similarity = find_similarity(my_taste, taste_list[profile])
 #    print ("similarity with '"+ profile +"': " + str(similarity))
@@ -198,36 +213,39 @@ for profile in taste_list:
         largest_similarity = similarity
         most_similar_taste = taste_list[profile]
         most_similar_profile = profile
-print("Most similar profile: ", most_similar_profile)
-print("Weight: ", largest_similarity)
-print()
+print("Most similar profile: " + str(most_similar_profile))
+print("Weight: " + str(largest_similarity))
+print("")
 
 ## Search the most_similar_taste for all the songs NOT in my_taste
-reccomended_songs = {}
-highest_reccomended = 0
+recommended_songs = {}
+highest_recommended = 0
 i = 0
 for song in most_similar_taste:
     if (song not in my_taste):
         similarity_weight = most_similar_taste[song]*largest_similarity
-        reccomended_songs[song] = similarity_weight
-#        print("[" + song + "]: " + str(reccomended_songs[song]))
-        highest_reccomended = highest_reccomended if highest_reccomended > similarity_weight else similarity_weight
+        recommended_songs[song] = similarity_weight
+#        print("[" + song + "]: " + str(recommended_songs[song]))
+        highest_recommended = highest_recommended if highest_recommended > similarity_weight else similarity_weight
         
-## find most reccomended songs and work our way down
-print ("Reccomended Songs For You:")
+## find most recommended songs and work our way down
 
-for song in reccomended_songs:
-    if reccomended_songs[song] > 2.0:
-        print("\"" + songs_by_id[song]['name'] + "\" by " + songs_by_id[song]['artist'] + " [weight of " + str(reccomended_songs[song]) +"]")
-    
+recommend_threshhold = .25
+if (recommend_threshhold <= highest_recommended):
+    print ("Recommended Songs For You:")
+    for song in collections.OrderedDict(sorted(recommended_songs.iteritems(), key=lambda x: x[1], reverse=True)):
+        if recommended_songs[song] > .2:
+            print("\"" + songs_by_id[song]['name'] + "\" by " + songs_by_id[song]['artist'] + " [weight of {0:.2f}".format(recommended_songs[song]) +"]")
+else: 
+    print ("No songs to recommend for you (threshhold: " + str(recommend_threshhold) + ")")
+                
 plot_tastes(my_taste, most_similar_taste)
 
 
-
 plt.figure()
-plot_taste(reccomended_songs, 'r+')
+plot_taste(recommended_songs, 'r+')
 plt.ylim(ymin = 0)
-plt.ylim(ymax = highest_reccomended + 1)
+plt.ylim(ymax = highest_recommended + 1)
 plt.show()
 
 
